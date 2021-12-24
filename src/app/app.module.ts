b265ce3abf3c9c542538e001d64256f74832ef09
logodi06 <lore.gomez.06@gmail.com>
2021-12-24 02:05:10 -0600

    crear aplicacion de superHeroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/interfaces/heroes.interface.ts
@@ -0,0 +1,15 @@
+export interface Heroe {
+    id?:               string;
+    superhero:        string;
+    publisher:        Publisher;
+    alter_ego:        string;
+    first_appearance: string;
+    characters:       string;
+    alt_img?:         string; //Va ser el link https://imagenes.com/img.png
+
+}
+
+export enum Publisher {
+    DCComics = "DC Comics",
+    MarvelComics = "Marvel Comics",
+}
